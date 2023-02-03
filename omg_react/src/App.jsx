18a0745f07c64a070a12db63680a52839e02230c
Diego Cano <82411053+Dicv987@users.noreply.github.com>
2023-02-03 15:39:38 -0600

    1.0
    Inicio Proyecto

---
+++ omg_react/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import {MyRoutes} from './routers/routes'
+
+function App(){
+  return(
+    <div>
+      <header>
+        <h1>Prueba Header</h1>
+      </header>
+      <MyRoutes/>
+    </div>
+  );
+}
+export default App
