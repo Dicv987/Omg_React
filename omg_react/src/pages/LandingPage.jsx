aa31b1c601335a0c5147bd66206769b142a9fb69
Diego Cano <82411053+Dicv987@users.noreply.github.com>
2023-02-08 00:07:11 -0600

    Funcionando TODO
    Menos navheader y el contact form

--- omg_react/src/pages/LandingPage.jsx
+++ omg_react/src/pages/LandingPage.jsx
@@ -18,12 +18,7 @@ export function LandingPage() {
         <MisVisVal />
       </main>
       <Footer />
-      <a
-        href="#"
-        className="back-to-top d-flex align-items-center justify-content-center"
-      >
-        <i className="bi bi-arrow-up-short"></i>
-      </a>
+
     </>
   );
 }
